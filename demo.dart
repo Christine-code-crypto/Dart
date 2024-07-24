bc8c115b09a100eee4e7f8bcead7b7bcdc658a5e
Christine Kerubo <kerubochri@gmail.com>
2024-07-24 22:15:16 +0300

    Getting input from a user

---
+++ demo.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+
+void main() {
+  stdout.writeln("What is your name?");
+  String name = stdin.readLineSync() ?? '';  // Handle the null case
+  if (name.isEmpty) {
+    name = 'Guest';  // Handle the empty string case
+  }
+  print("My name is $name");
+}
+
